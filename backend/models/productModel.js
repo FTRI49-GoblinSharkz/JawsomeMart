efc04e1d7d35ebd1f3cfe8696ef1392264313c2a
Eric DiMarzio <127116933+EricDiMarzio@users.noreply.github.com>
2024-08-07 12:15:16 -0500

    Product Controller: addItem, deleteItem, changeStock, changeSold Product Model: Added stock and num_sold to schema.

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,13 +3,15 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema(
   {
-    id: { type: Number, required: true, unique: true },
+    id: { type: Number, required: true, unique: true, default: Math.ceil(Math.random()*10000) },
     title: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
     rating: { type: String, required: true },
+    stock: {type: Number, required: true, default: 10},
+    num_sold: {type: Number, required: true, default: 0}
   },
   {
     timestamps: true,
@@ -19,3 +21,6 @@ const productSchema = new Schema(
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
+
+//Issue: generating ID for new products. Is this field redundant with the _id field?
+//Temporary solution: Random number between 1 and 10,000. UUID does not work because it includes alpha characters
